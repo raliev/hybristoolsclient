f573d34a555ddc432cbf8eb72c601054cfdc50c3
Maxim Antonov <max.antonoff@gmail.com>
2017-01-26 17:59:06 +0300

    fix maven project and unix starter script for hybrisConfiguration

--- console/hybrisConfiguration.sh
+++ console/hybrisConfiguration.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -classpath "../lib/commons-logging-1.2.jar;../lib/httpclient-4.5.2.jar;../lib/httpmime-4.5.2.jar;../lib/jcommander-1.48.jar;../lib/rxjava-1.1.6.jar;../target/hybristools-1.0-SNAPSHOT.jar;../lib/httpcore-4.4.4.jar" com.epam.hybristoolsclient.HybrisConfiguration "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "${10}" "${11}" "${12}" "${13}" "${14}" "${15}" 
+java  -jar ../target/hybristools-1.0-SNAPSHOT.jar com.epam.hybristoolsclient.HybrisConfiguration "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "${10}" "${11}" "${12}" "${13}" "${14}" "${15}" 
