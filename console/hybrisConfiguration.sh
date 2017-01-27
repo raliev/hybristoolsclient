6ce2b82c7285eaa1769b02422a5e7bce488dbecb
Maxim Antonov <max.antonoff@gmail.com>
2017-01-27 21:47:06 +0300

    fix configuration starter script

--- console/hybrisConfiguration.sh
+++ console/hybrisConfiguration.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java  -jar ../target/hybristools-1.0-SNAPSHOT.jar com.epam.hybristoolsclient.HybrisConfiguration "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "${10}" "${11}" "${12}" "${13}" "${14}" "${15}" 
+java -cp ../target/hybristools-1.0-SNAPSHOT.jar com.epam.hybristoolsclient.HybrisConfiguration "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "${10}" "${11}" "${12}" "${13}" "${14}" "${15}"
